8e9625c251e5e3155bd05cdd3c7c3e4f5fb65700
Seisuke Yamada <momoiropuchoman@gmail.com>
2025-06-29 19:25:24 +0900

    refactor: organize the codebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
-import { Button } from "@/components/ui/button";
+export const dynamic = "force-dynamic";
+
 import { getWords } from "@/notion/get-words";
 import Image from "next/image";
 import { use } from "react";
 
 export default function Home() {
   use(getWords());
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Button variant="destructive">This is a Tailwind styled button</Button>
         <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
@@ -16,7 +17,7 @@ export default function Home() {
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               app/page.tsx
             </code>
-            .
+            .c
           </li>
           <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
         </ol>
--- notion/get-words.ts
+++ notion/get-words.ts
@@ -5,6 +5,8 @@ export async function getWords() {
     auth: process.env.NOTION_API_KEY,
   });
 
-  const response = await notion.databases.query({ database_id: process.env.NOTION_DATABSE_ID_WORDS });
+  const response = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID_WORDS ?? "",
+  });
   console.log(response);
 }
