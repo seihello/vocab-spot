6cfc5444f4c84e22bde321795906dd42b7252756
Seisuke Yamada <momoiropuchoman@gmail.com>
2025-07-21 19:56:55 +0900

    improvement: fetch all words from Notion

--- lib/notion/get-arrayed-words.ts
+++ lib/notion/get-arrayed-words.ts
@@ -6,60 +6,68 @@ export async function getArrayedWords() {
     auth: process.env.NOTION_API_KEY,
   });
 
-  const response = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID_WORDS ?? "",
-  });
-
-  const results = response.results as PageObjectResponse[];
-
+  let hasMore = true;
   const words: ArrayedWord[] = [];
+  let startCursor: string | undefined = undefined;
 
-  results.forEach((result) => {
-    const names = result.properties.Names;
-    const meanings = result.properties.Meanings;
-    const sentences = result.properties.Sentences;
-    const collocations = result.properties.Collocations;
-    const tags = result.properties.Tags;
-    const pronunciations = result.properties.Pronunciations;
-    const level = result.properties.Level;
-    if (
-      names.type === "rich_text" &&
-      meanings.type === "rich_text" &&
-      sentences.type === "rich_text" &&
-      collocations.type === "rich_text" &&
-      tags.type === "multi_select" &&
-      pronunciations.type === "rich_text" &&
-      level.type === "number" &&
-      names.rich_text.length > 0
-    ) {
-      const splitNames = names.rich_text.flatMap((name) => name.plain_text.split("\n")).filter((name) => name !== "");
-      const splitMeanings = meanings.rich_text
-        .flatMap((meaning) => meaning.plain_text.split("\n"))
-        .filter((meaning) => meaning !== "");
-      const splitSentences = sentences.rich_text
-        .flatMap((sentence) => sentence.plain_text.split("\n"))
-        .filter((sentence) => sentence !== "");
-      const splitCollocations = collocations.rich_text
-        .flatMap((collocation) => collocation.plain_text.split("\n"))
-        .filter((collocation) => collocation !== "");
-      const splitPronunciations = pronunciations.rich_text
-        .flatMap((pronunciation) => pronunciation.plain_text.split("\n"))
-        .filter((pronunciation) => pronunciation !== "");
-      const splitTags = tags.multi_select.map((tag) => tag.name);
+  while (hasMore) {
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID_WORDS ?? "",
+      start_cursor: startCursor ?? undefined,
+    });
 
-      const word = {
-        id: result.id,
-        names: splitNames,
-        meanings: splitMeanings.length > 0 ? splitMeanings : null,
-        sentences: splitSentences.length > 0 ? splitSentences : null,
-        collocations: splitCollocations.length > 0 ? splitCollocations : null,
-        pronunciations: splitPronunciations.length > 0 ? splitPronunciations : null,
-        tags: splitTags.length > 0 ? splitTags : null,
-        level: level.number,
-      };
-      words.push(word);
-    }
-  });
-  console.log(words);
+    const results = response.results as PageObjectResponse[];
+
+    results.forEach((result) => {
+      const names = result.properties.Names;
+      const meanings = result.properties.Meanings;
+      const sentences = result.properties.Sentences;
+      const collocations = result.properties.Collocations;
+      const tags = result.properties.Tags;
+      const pronunciations = result.properties.Pronunciations;
+      const level = result.properties.Level;
+      if (
+        names.type === "rich_text" &&
+        meanings.type === "rich_text" &&
+        sentences.type === "rich_text" &&
+        collocations.type === "rich_text" &&
+        tags.type === "multi_select" &&
+        pronunciations.type === "rich_text" &&
+        level.type === "number" &&
+        names.rich_text.length > 0
+      ) {
+        const splitNames = names.rich_text.flatMap((name) => name.plain_text.split("\n")).filter((name) => name !== "");
+        const splitMeanings = meanings.rich_text
+          .flatMap((meaning) => meaning.plain_text.split("\n"))
+          .filter((meaning) => meaning !== "");
+        const splitSentences = sentences.rich_text
+          .flatMap((sentence) => sentence.plain_text.split("\n"))
+          .filter((sentence) => sentence !== "");
+        const splitCollocations = collocations.rich_text
+          .flatMap((collocation) => collocation.plain_text.split("\n"))
+          .filter((collocation) => collocation !== "");
+        const splitPronunciations = pronunciations.rich_text
+          .flatMap((pronunciation) => pronunciation.plain_text.split("\n"))
+          .filter((pronunciation) => pronunciation !== "");
+        const splitTags = tags.multi_select.map((tag) => tag.name);
+
+        const word = {
+          id: result.id,
+          names: splitNames,
+          meanings: splitMeanings.length > 0 ? splitMeanings : null,
+          sentences: splitSentences.length > 0 ? splitSentences : null,
+          collocations: splitCollocations.length > 0 ? splitCollocations : null,
+          pronunciations: splitPronunciations.length > 0 ? splitPronunciations : null,
+          tags: splitTags.length > 0 ? splitTags : null,
+          level: level.number,
+        };
+        words.push(word);
+      }
+    });
+
+    hasMore = response.has_more;
+    startCursor = response.next_cursor ?? undefined;
+  }
+  // console.log(words);
   return words;
 }
