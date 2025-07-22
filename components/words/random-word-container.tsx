2d77391b74d3e4aec413bd929ed9e3f131c47e3b
Seisuke Yamada <momoiropuchoman@gmail.com>
2025-07-23 02:04:26 +0900

    feat: hide/show meanings and sentences

--- components/words/random-word-container.tsx
+++ components/words/random-word-container.tsx
@@ -14,14 +14,21 @@ export default function RandomWordContainer({ wordIds }: Props) {
   const [words, setWords] = useState<Word[]>([]);
   const [currentIndex, setCurrentIndex] = useState(-1);
   const [isFetchingWord, setIsFetchingWord] = useState(false);
+  const [isDetailHidden, setIsDetailHidden] = useState(true);
+
+  const onClickShowAnswer = () => {
+    setIsDetailHidden(false);
+  };
 
   const onClickPrev = () => {
+    setIsDetailHidden(true);
     setCurrentIndex((prev) => (prev > 0 ? prev - 1 : prev));
   };
 
   const onClickNext = useCallback(async () => {
     if (isFetchingWord) return;
     if (currentIndex + 1 < words.length) {
+      setIsDetailHidden(true);
       setCurrentIndex((prev) => prev + 1);
       return;
     }
@@ -31,6 +38,7 @@ export default function RandomWordContainer({ wordIds }: Props) {
     const randomId = wordIds[Math.floor(Math.random() * wordIds.length)];
     const word = await getWordById(randomId);
     setWords((prev) => [...prev, word]);
+    setIsDetailHidden(true);
     setCurrentIndex((prev) => prev + 1);
 
     setIsFetchingWord(false);
@@ -45,14 +53,19 @@ export default function RandomWordContainer({ wordIds }: Props) {
   if (words.length <= 0) return;
 
   return (
-    <div className="flex flex-col sm:flex-row gap-x-16 items-center justify-center w-full m-auto pt-8 sm:pt-16 pb-4 px-2 sm:px-8 gap-y-2 min-h-dvh sm:min-h-auto">
-      <Button onClick={onClickPrev} className="order-1 sm:order-0" disabled={currentIndex === 0}>
-        Prev
-      </Button>
-      <RandomWord word={words[currentIndex]} />
-      <Button onClick={onClickNext} className="order-last" disabled={isFetchingWord}>
-        Next
-      </Button>
+    <div className="flex flex-col items-end justify-center w-full max-w-256 mx-auto pt-8 pb-4 px-2 sm:px-8 gap-y-2 min-h-dvh sm:min-h-auto">
+      <RandomWord word={words[currentIndex]} isDetailHidden={isDetailHidden} />
+      <div className="w-full sm:w-auto flex flex-col sm:flex-row gap-2 sm:-order-1">
+        <Button variant="outline" onClick={onClickShowAnswer} disabled={!isDetailHidden}>
+          Show Answer
+        </Button>
+        <Button onClick={onClickPrev} disabled={currentIndex === 0}>
+          Prev
+        </Button>
+        <Button onClick={onClickNext} disabled={isFetchingWord}>
+          Next
+        </Button>
+      </div>
     </div>
   );
 }
--- components/words/random-word.tsx
+++ components/words/random-word.tsx
@@ -3,14 +3,19 @@ import React from "react";
 
 type Props = {
   word: Word;
+  isDetailHidden: boolean;
 };
 
-export default function RandomWord({ word }: Props) {
+export default function RandomWord({ word, isDetailHidden }: Props) {
   return (
-    <div className="space-y-2 whitespace-pre-line grow w-full sm:w-auto">
+    <div className="space-y-2 whitespace-pre-line grow w-full">
       <div className="font-bold text-2xl text-primary-700">{word.names}</div>
-      <div className="text-gray-500">{word.meanings}</div>
-      <div className="">{word.sentences}</div>
+      {!isDetailHidden && (
+        <>
+          <div className="text-gray-500">{word.meanings}</div>
+          <div>{word.sentences}</div>
+        </>
+      )}
     </div>
   );
 }
