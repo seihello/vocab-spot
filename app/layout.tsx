c358c41a626ae78eda1c0bc8a452e1ce0ab6952a
Seisuke Yamada <momoiropuchoman@gmail.com>
2025-07-29 00:23:19 +0900

    style: remove theme color

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
-        <meta name="theme-color" content="#b8e986" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
