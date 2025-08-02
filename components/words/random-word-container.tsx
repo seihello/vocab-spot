06797cc360a6f81416fc60a0eec9f124b1ac80c1
Seisuke Yamada <momoiropuchoman@gmail.com>
2025-08-03 01:49:00 +0900

    style: adjust for better layout

--- components/words/random-word-container.tsx
+++ components/words/random-word-container.tsx
@@ -84,28 +84,29 @@ export default function RandomWordContainer({ tags }: Props) {
   const isReady = words.length > 0 && currentIndex >= 0;
 
   return (
-    <div
-      className={`flex flex-col items-end justify-center w-full max-w-256 mx-auto pt-8 px-2 sm:px-8 gap-y-2 min-h-dvh sm:min-h-auto ${
-        isPwa ? "pb-16" : "pb-4"
-      }`}
-    >
-      <div className="w-full grow">
+    <div className="max-h-screen flex flex-col items-end justify-center w-full max-w-256 mx-auto pt-8 px-2 sm:px-8 gap-y-2 min-h-dvh sm:min-h-auto">
+      <div className="w-full grow overflow-y-scroll">
         {isReady && <RandomWord word={words[currentIndex]} isDetailHidden={isDetailHidden} />}
+        <div className="w-full bg-yellow-50">
+          {messages
+            .filter((message) => message.role === "assistant")
+            .map((message) => (
+              <div key={message.id}>
+                {message.parts
+                  .filter((part) => part.type === "text")
+                  .map((part, index) => (
+                    <span key={index}>{part.text}</span>
+                  ))}
+              </div>
+            ))}
+        </div>
       </div>
-      <div>
-        {messages
-          .filter((message) => message.role === "assistant")
-          .map((message) => (
-            <div key={message.id}>
-              {message.parts
-                .filter((part) => part.type === "text")
-                .map((part, index) => (
-                  <span key={index}>{part.text}</span>
-                ))}
-            </div>
-          ))}
-      </div>
-      <div className="w-full sm:w-auto flex flex-col sm:flex-row gap-2 sm:-order-1 items-end sm:items-center">
+
+      <div
+        className={`w-full px-2 bg-white sm:w-auto flex flex-col sm:flex-row gap-2 sm:-order-1 items-end sm:items-center ${
+          isPwa ? "pb-16" : "pb-4"
+        }`}
+      >
         <Menu tagOptions={tags} defaultSelectedTags={selectedTags} onUpdate={onUpdateSelectedTags} />
         <Button variant="outline" onClick={onClickGenerateSentence} disabled={!isReady}>
           Generate Sentence
@@ -113,15 +114,18 @@ export default function RandomWordContainer({ tags }: Props) {
         <Button variant="outline" onClick={onClickShowAnswer} disabled={!isReady || !isDetailHidden}>
           Show Answer
         </Button>
-        <Button onClick={onClickPrev} disabled={!isReady || currentIndex === 0}>
-          Prev
-        </Button>
-        <Button
-          onClick={onClickNext}
-          disabled={!isReady || isFetchingWord || (currentIndex === words.length - 1 && isFinalWord)}
-        >
-          Next
-        </Button>
+        <div className="flex w-full gap-x-2">
+          <Button onClick={onClickPrev} disabled={!isReady || currentIndex === 0} className="flex-1">
+            Prev
+          </Button>
+          <Button
+            onClick={onClickNext}
+            disabled={!isReady || isFetchingWord || (currentIndex === words.length - 1 && isFinalWord)}
+            className="flex-1"
+          >
+            Next
+          </Button>
+        </div>
       </div>
     </div>
   );
