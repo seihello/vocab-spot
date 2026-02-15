c13a5637969701dee988da75c066886b5e6fc677
Seisuke Yamada <momoiropuchoman@gmail.com>
2026-02-15 16:19:39 +0900

    feat: connect with firestore

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import addWords from "@/lib/firebase/add-words";
 import { Word } from "@/lib/types";
 import { validatePasscode } from "@/lib/validate-passcode";
 import { useEffect, useState } from "react";
@@ -28,7 +29,7 @@ export default function Home() {
       <Input type="password" placeholder="Enter passcode" onChange={(e) => setPasscode(e.target.value)} />
       {isPasscodePassed && (
         <div className="fade-in flex gap-x-2 w-full">
-          <Button>No Action</Button>
+          <Button onClick={async () => await addWords()}>Add word test</Button>
         </div>
       )}
       <div className="space-y-4">
---
+++ lib/firebase/get-client.ts
@@ -0,0 +1,18 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+// サーバーサイドでの二重初期化を防ぐ
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db as firestore };
